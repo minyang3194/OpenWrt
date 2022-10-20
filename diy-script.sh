a2e7073d5aaf9e3ff3529c4285ee1eb488cb1ebc
minyang3194 <50267545+minyang3194@users.noreply.github.com>
2022-10-20 22:54:53 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -8,7 +8,7 @@
 #===============================================
 
 # 修改默认IP
-# sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.1168.2.99/g' package/base-files/files/bin/config_generate
 
 # Change default shell to zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
